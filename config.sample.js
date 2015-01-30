74dc519698f59cd2f29f54c98891e1ddb1d85eed
Fntsrlike <fntsrlike@gmail.com>
2015-01-30 23:43:45 +0800

    Add response action to slack user

--- config.sample.js
+++ config.sample.js
@@ -5,6 +5,7 @@ var App = require('./lib/app');
 var config = {
   fileName: './map.json',
   token: 'XXXXXXXXXXXXXXXXXXXXXXXX',
+  incomeUrl: 'https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX',
   port: 80
 };
 
